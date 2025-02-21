6e5e42f34ae6ef205f2d0e8fac958b00ed35d4fa
hungnqdz <hungminion0330@gmail.com>
2025-02-21 16:59:48 +0700

    First commit

---
+++ config-outer-gw.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+ip route del default via 192.168.30.1
+ip route add default via 172.18.0.1
+iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to-destination 192.168.30.100:80
+iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
---
+++ config-ws2.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+ip route del default via 192.168.10.1
+ip route add default via 192.168.10.2
