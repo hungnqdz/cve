034ecb6add8dc3e780e0cf246195f270c0d3f70a
root <root@LAPTOP-I2MKA6E0>
2025-04-17 20:23:18 +0700

    update

--- activemq_server/config-web-server.sh
+++ activemq_server/config-web-server.sh
@@ -3,3 +3,4 @@ ip route del default
 ip route add default via 192.168.30.2
 apache2ctl start
 service rsyslog start
+/usr/sbin/sshd -D
--- outer_gw/config-outer-gw.sh
+++ outer_gw/config-outer-gw.sh
@@ -3,5 +3,7 @@ ip route del default via 192.168.30.1
 ip route add default via 172.18.0.1
 iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to-destination 192.168.30.100:80
 iptables -t nat -A PREROUTING -p tcp --dport 61616 -j DNAT --to-destination 192.168.30.100:61616
+iptables -t nat -A PREROUTING -p tcp --dport 22 -j DNAT --to-destination 192.168.30.100:22
+#iptables -t nat -A POSTROUTING -d 192.168.30.100 -p tcp --dport 22 -j MASQUERADE
 iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
